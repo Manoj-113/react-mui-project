ea0522ab29eb7410c8f21d381310eabf004c5436
Manoj-113 <manojanuradha00@gmail.com>
2021-10-29 11:05:21 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import React from "react";
 import { Person } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  button: {
+    color: "white",
+    backgroundColor: "black",
+  },
+});
 
 const App = () => {
-  return <div>
-    <Button variant = "contained" startIcon ={<Person/>}>Material UI</Button>
-  </div>
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        startIcon={<Person />}
+        className={classes.button}
+      >
+        Material UI
+      </Button>
+    </div>
+  );
 };
 
 export default App;
