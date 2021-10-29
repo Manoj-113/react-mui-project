7dcb3c1fabb6f798460fcbf6a1229dad95ab4b38
Manoj-113 <manojanuradha00@gmail.com>
2021-10-29 13:34:35 +0530

    Updated

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { blue } from "@mui/material/colors";
+import { createTheme } from "@mui/system";
+
+export const theme = createTheme = createTheme({
+    palette:{
+        primary:{
+            main: blue[500],
+        }
+    }
+})
