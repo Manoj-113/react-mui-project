3cebcb70fafa3ca860f7d99f7ffc9cf848d7c896
Manoj-113 <manojanuradha00@gmail.com>
2021-10-29 13:23:03 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Person } from "@material-ui/icons-material";
-import { Button } from "@material-ui/material";
-import { makeStyles } from "@material-ui/core/styles";
+import { Person } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { makeStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   button: {
     color: "white",
-    backgroundColor: "black",
+    backgroundColor: theme.palette.success.main,
   },
-});
+}));
 
 const App = () => {
   const classes = useStyles();
